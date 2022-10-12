cb45e8f532b33b91c1164852ea0640b743b6e27d
minervaDutta <dutta.minerva@gmail.com>
2022-10-12 01:00:27 +0000

    personal final project

--- src/app.js
+++ src/app.js
@@ -9,3 +9,25 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function randomGenerator() {
+  var whoexcuse = who[Math.floor(Math.random() * who.length)];
+  var actionexcuse = action[Math.floor(Math.random() * action.length)];
+  var whatexcuse = what[Math.floor(Math.random() * what.length)];
+  var whenexcuse = when[Math.floor(Math.random() * when.length)];
+
+  return whoexcuse + " " + actionexcuse + " " + whatexcuse + " " + whenexcuse;
+}
+//document.write(randomGenerator());
+document.getElementById("excuse").innerHTML = randomGenerator();
